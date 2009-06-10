48379c1f3657d403c53383ba9e35e6957bd0c9be
Thibaut Barrère <thibaut.barrere@gmail.com>
2009-06-10 17:21:33 +0200

    Implement patch apply

--- lib/data_patcher.rb
+++ lib/data_patcher.rb
@@ -11,21 +11,40 @@ class DataPatcher
     @key = key
     @patch = {}
   end
-    
+
+  # attributes hash must include key
+  def find_model(attributes)
+    model.find(:first, :conditions => { key => attributes[key] })
+  end
+  
   def analyze_record(attributes)
-    existing = model.find(:first, :conditions => { key => attributes[key] })
+    existing = find_model(attributes)
     if existing
       existing_attributes = existing.attributes.dup.symbolize_keys.except(:id)
       diff = attributes.diff(existing_attributes)
       # always include the key in the mix so that a look-up is possible when applying the patch
       unless diff.empty?
-        patch[:modified] ||= []
         diff[key] = attributes[key]
-        patch[:modified] << diff
+        modifications << diff
       end
     else
-      patch[:added] ||= []
-      patch[:added] << attributes
+      additions << attributes
+    end
+  end
+  
+  def additions
+    patch[:added] ||= []
+  end
+  
+  def modifications
+    patch[:modified] ||= []
+  end
+  
+  def apply!
+    additions.each { |attributes| model.create!(attributes) }
+    modifications.each do |attributes|
+      instance = find_model(attributes)
+      raise "Failed to find or update #{model} with <#{key}> = <#{key_value}>" if instance.nil? || !instance.update_attributes(attributes)
     end
   end
   
--- spec/data_patcher_spec.rb
+++ spec/data_patcher_spec.rb
@@ -11,15 +11,23 @@ describe DataPatcher do
     @patcher.analyze_record(@existing_item_2)
   end
   
-  it "provides an addition patch including the key and the attributes" do
+  it "provides an addition patch including the key and the attributes, and knows how to apply it" do
     @patcher.analyze_record({ :asin => "123", :title => "Hello", :summary => "The best of hello world" } )
 
     @patcher.patch.should == {
       :added => [{ :asin => "123", :title => "Hello", :summary => "The best of hello world" }]
     }
+    
+    lambda {
+      @patcher.apply!
+      item = Item.find(:last)
+      item.asin.should == "123"
+      item.title.should == "Hello"
+      item.summary.should == "The best of hello world"
+    }.should change(Item, :count).by(1)
   end
   
-  it "provides a modification patch including the key and updated attributes" do
+  it "provides a modification patch including the key and updated attributes, and knows how to apply it" do
     @patcher.analyze_record( { 
       :asin => "0471777099", # key
       :title => "Mastering Data Warehouse Aggregates (Hard cover)", # changed data
@@ -28,6 +36,15 @@ describe DataPatcher do
     @patcher.patch.should == {
       :modified => [{ :asin => "0471777099", :title => "Mastering Data Warehouse Aggregates (Hard cover)"}]
     }
+
+    item = Item.find_by_asin("0471777099")
+
+    lambda {
+      @patcher.apply!
+      item.reload
+    }.should change(item, :title).
+      from("Mastering Data Warehouse Aggregates").
+      to("Mastering Data Warehouse Aggregates (Hard cover)")
   end
   
 end
